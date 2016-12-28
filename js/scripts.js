53b7cd1daf2c0ed63d0bfdba8d0bf275c4cdb8c4
chris martinez <chris martinez>
2016-12-28 01:17:59 -0800

    component moves with mouse

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ var myGameArea = {
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
+        this.canvas.style.cursor = "none";
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -24,6 +25,10 @@ var myGameArea = {
          })
          window.addEventListener('keyup', function (e) {
              myGameArea.keys[e.keyCode] = false;
+         });
+         window.addEventListener('mousemove', function(e){
+           myGameArea.x = e.pageX;
+           myGameArea.y = e.pageY
          })
     },
     clear : function() {
@@ -60,7 +65,10 @@ function updateGameArea() {
     if(myGameArea.keys && myGameArea.keys[39] == true) {myGamePiece.speedX = 1 }
     if(myGameArea.keys && myGameArea.keys[38] == true) {myGamePiece.speedY = -1 }
     if(myGameArea.keys && myGameArea.keys[40] == true) {myGamePiece.speedY = 1 }
-
+    if(myGameArea.x && myGameArea.y){
+      myGamePiece.x = myGameArea.x;
+      myGamePiece.y = myGameArea.y;
+    }
     //
     // if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
     // if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
