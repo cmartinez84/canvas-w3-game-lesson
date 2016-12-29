6f179f936ea7da875633e8543c7e560f647bd895
chris martinez <chris martinez>
2016-12-28 22:02:34 -0800

    stop all functionality when red square meets green, keyboard contorls disabled due to conflict with controllers

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 var myGamePiece;
+var myObstacle;
 
 function startGame() {
-    myGameArea.start();
-    myGamePiece = new component(30, 10, "red", 10, 120);
-
-    redGamePiece = new component(75, 75, "red", 10, 10);
-    yellowGamePiece = new component(75, 75, "yellow", 50, 60);
-    blueGamePiece = new component(75, 75, "blue", 10, 110);
-
+  myGameArea.start();
+    myGamePiece = new component(20, 10, "red", 10, 120);
+    myObstacle = new component(10, 200, "green", 300, 120);
 }
 
 var myGameArea = {
@@ -15,25 +12,28 @@ var myGameArea = {
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
-        this.canvas.style.cursor = "none";
+        // this.canvas.style.cursor = "none";
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('keydown', function (e) {
-             myGameArea.keys = (myGameArea.keys || []);
-             myGameArea.keys[e.keyCode] = true;
-         })
-         window.addEventListener('keyup', function (e) {
-             myGameArea.keys[e.keyCode] = false;
-         });
-         window.addEventListener('mousemove', function(e){
-           myGameArea.x = e.pageX;
-           myGameArea.y = e.pageY
-         })
+        // window.addEventListener('keydown', function (e) {
+        //      myGameArea.keys = (myGameArea.keys || []);
+        //      myGameArea.keys[e.keyCode] = true;
+        //  })
+        //  window.addEventListener('keyup', function (e) {
+        //      myGameArea.keys[e.keyCode] = false;
+        //  });
+        //  window.addEventListener('mousemove', function(e){
+        //    myGameArea.x = e.pageX;
+        //    myGameArea.y = e.pageY
+        //  })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
+    stop : function (){
+      clearInterval(this.interval);
+    }
 }
 
 function component(width, height, color, x, y) {
@@ -53,14 +53,43 @@ function component(width, height, color, x, y) {
     this.newPos = function (){
       this.x += this.speedX;
       this.y += this.speedY;
+    },
+    this.crashWith = function(otherobj){
+      var myleft = this.x;
+      var myright = this.x + (this.width);
+      var mytop = this.y;
+      var mybottom = this.y + (this.height);
+      var otherleft = otherobj.x;
+      var otherright = otherobj.x + (otherobj.width);
+      var othertop = otherobj.y;
+      var otherbottom = otherobj.y + (otherobj.y + otherobj.height);
+      var crash = true;
+      if( (mybottom < othertop) ||
+          (mytop > otherbottom) ||
+          (myright < otherleft) ||
+          (myleft > otherright)){
+              crash = false;
+          }
+      return crash;
     }
-}
+  }
+
+
+
 function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.newPos();
-    myGamePiece.update();
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
+    // myGamePiece.speedX = 0;
+    // myGamePiece.speedY = 0;
+    if (myGamePiece.crashWith (myObstacle)){
+      myGameArea.stop();
+    }
+    else {
+      myGameArea.clear();
+      myObstacle.update();
+      myGamePiece.newPos();
+      myGamePiece.update();
+    }
+
+
     if(myGameArea.keys && myGameArea.keys[37] == true) {myGamePiece.speedX = -1 }
     if(myGameArea.keys && myGameArea.keys[39] == true) {myGamePiece.speedX = 1 }
     if(myGameArea.keys && myGameArea.keys[38] == true) {myGamePiece.speedY = -1 }
@@ -69,23 +98,13 @@ function updateGameArea() {
       myGamePiece.x = myGameArea.x;
       myGamePiece.y = myGameArea.y;
     }
-    //
+    // for use with keyboard, conflicts with controls on screen
     // if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
     // if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
     // if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -1; }
     // if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 1; }
 
 
-
-    // redGamePiece.x += 1;
-    // yellowGamePiece.x += 1;
-    // yellowGamePiece.y += 1;
-    // blueGamePiece.x += 1;
-    // blueGamePiece.y -= 1;
-
-  //  redGamePiece.update();
-  //  yellowGamePiece.update();
-  //  blueGamePiece.update();
 }
 function moveUp(){
   myGamePiece.speedY -= 1;
